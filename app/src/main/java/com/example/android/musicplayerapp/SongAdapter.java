37844ea7f49e11700353cb645258430f9c69bb38
Juliet Antipolo <j.antipolo@yahoo.com>
2018-06-20 16:37:14 -0400

    added images for diff resolutions and updated credits and song details page

--- app/src/main/java/com/example/android/musicplayerapp/SongAdapter.java
+++ app/src/main/java/com/example/android/musicplayerapp/SongAdapter.java
@@ -14,8 +14,23 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-/**
- * Created by julietantipolo on 6/16/18.
+
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Modified by julietantipolo on 6/16/18.
  */
 
 
